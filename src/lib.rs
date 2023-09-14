5c25bd6f9d9e983d2f41ad9a718d21dcdfe038ea
splurf <evandotschwartzentruber@gmail.com>
2023-09-14 15:30:32 -0400

    shortened docs and some refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,7 @@ mod home_dir_windows {
         },
     };
 
-    /// Return the user's home directory.
-    ///
-    /// ```
-    /// use simple_home_dir::home_dir;
-    ///
-    /// let path = home_dir().unwrap();
-    /// ```
+    /// Return the path of the user's home directory.
     pub fn home_dir() -> Option<PathBuf> {
         let mut path_ptr = null_mut();
         (unsafe { SHGetKnownFolderPath(&FOLDERID_Profile, 0, 0, &mut path_ptr) } == 0).then_some({
@@ -38,16 +32,9 @@ pub use home_dir_windows::*;
 #[cfg(not(target_family = "windows"))]
 mod home_dir_ne_windows {
     use std::{env::var_os, path::PathBuf};
-
     const HOME: &'static str = "HOME";
 
-    /// Return the user's home directory.
-    ///
-    /// ```
-    /// use simple_home_dir::home_dir;
-    ///
-    /// let path = home_dir().unwrap();
-    /// ```
+    /// Return the path of the user's home directory.
     pub fn home_dir() -> Option<PathBuf> {
         var_os(HOME).map(Into::into)
     }
@@ -61,18 +48,9 @@ mod expand_tilde {
         super::home_dir,
         std::path::{Path, PathBuf},
     };
-
     const TILDE: &'static str = "~";
 
-    /// Expand the tilde that originates in the provided path.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use simple_home_dir::expand_tilde;
-    ///
-    /// let expanded = expand_tilde("~/.local").unwrap();
-    /// ```
+    /// Expand the tilde (`~`) from within the provided path.
     pub fn expand_tilde(path: impl AsRef<Path>) -> Option<PathBuf> {
         let p = path.as_ref();
 
@@ -98,8 +76,7 @@ mod test {
     mod expand_tilde {
         #[test]
         fn expand_tilde_test() {
-            let mut expected = dirs::home_dir().unwrap();
-            expected.push("foo");
+            let expected = dirs::home_dir().unwrap().join("foo");
             let expanded = crate::expand_tilde("~/foo").unwrap();
             assert_eq!(expanded, expected)
         }
