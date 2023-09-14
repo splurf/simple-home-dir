21560a3feafd9e91269b3a892cb54bbd3b5c04c7
splurf <evandotschwartzentruber@gmail.com>
2023-09-14 15:58:17 -0400

    fully modulable now

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,52 @@
-#[cfg(target_family = "windows")]
-mod home_dir_windows {
-    use {
-        core::slice::from_raw_parts,
-        std::{
-            ffi::{c_void, OsString},
-            os::windows::prelude::OsStringExt,
-            path::PathBuf,
-            ptr::null_mut,
-        },
-        windows_sys::Win32::{
-            Globalization::lstrlenW,
-            System::Com::CoTaskMemFree,
-            UI::Shell::{FOLDERID_Profile, SHGetKnownFolderPath},
-        },
-    };
+#[cfg(feature = "home_dir")]
+mod home_dir {
+    #[cfg(target_family = "windows")]
+    mod home_dir_windows {
+        use {
+            core::slice::from_raw_parts,
+            std::{
+                ffi::{c_void, OsString},
+                os::windows::prelude::OsStringExt,
+                path::PathBuf,
+                ptr::null_mut,
+            },
+            windows_sys::Win32::{
+                Globalization::lstrlenW,
+                System::Com::CoTaskMemFree,
+                UI::Shell::{FOLDERID_Profile, SHGetKnownFolderPath},
+            },
+        };
 
-    /// Return the path of the user's home directory.
-    pub fn home_dir() -> Option<PathBuf> {
-        let mut path_ptr = null_mut();
-        (unsafe { SHGetKnownFolderPath(&FOLDERID_Profile, 0, 0, &mut path_ptr) } == 0).then_some({
-            let wide = unsafe { from_raw_parts(path_ptr, lstrlenW(path_ptr) as usize) };
-            let ostr = OsString::from_wide(wide);
-            unsafe { CoTaskMemFree(path_ptr as *const c_void) }
-            ostr.into()
-        })
+        /// Return the path of the user's home directory.
+        pub fn home_dir() -> Option<PathBuf> {
+            let mut path_ptr = null_mut();
+            (unsafe { SHGetKnownFolderPath(&FOLDERID_Profile, 0, 0, &mut path_ptr) } == 0)
+                .then_some({
+                    let wide = unsafe { from_raw_parts(path_ptr, lstrlenW(path_ptr) as usize) };
+                    let ostr = OsString::from_wide(wide);
+                    unsafe { CoTaskMemFree(path_ptr as *const c_void) }
+                    ostr.into()
+                })
+        }
     }
-}
-#[cfg(target_family = "windows")]
-pub use home_dir_windows::*;
+    #[cfg(target_family = "windows")]
+    pub use home_dir_windows::*;
 
-#[cfg(not(target_family = "windows"))]
-mod home_dir_ne_windows {
-    use std::{env::var_os, path::PathBuf};
-    const HOME: &'static str = "HOME";
+    #[cfg(not(target_family = "windows"))]
+    mod home_dir_ne_windows {
+        use std::{env::var_os, path::PathBuf};
+        const HOME: &'static str = "HOME";
 
-    /// Return the path of the user's home directory.
-    pub fn home_dir() -> Option<PathBuf> {
-        var_os(HOME).map(Into::into)
+        /// Return the path of the user's home directory.
+        pub fn home_dir() -> Option<PathBuf> {
+            var_os(HOME).map(Into::into)
+        }
     }
+    #[cfg(not(target_family = "windows"))]
+    pub use home_dir_ne_windows::*;
 }
-#[cfg(not(target_family = "windows"))]
-pub use home_dir_ne_windows::*;
+#[cfg(feature = "home_dir")]
+pub use home_dir::*;
 
 #[cfg(feature = "expand_tilde")]
 mod expand_tilde {
