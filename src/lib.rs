13a81efe5e17c314a0c6bad02c7a75dc27525d68
splurf <evandotschwartzentruber@gmail.com>
2024-01-12 14:05:49 -0500

    refac => reduced code size

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,23 @@
 #[cfg(feature = "home_dir")]
 mod home_dir {
     #[cfg(target_family = "windows")]
-    mod home_dir_windows {
-        use {
-            core::slice::from_raw_parts,
-            std::{
-                ffi::{c_void, OsString},
-                os::windows::prelude::OsStringExt,
-                path::PathBuf,
-                ptr::null_mut,
-            },
-            windows_sys::Win32::{
-                Globalization::lstrlenW,
-                System::Com::CoTaskMemFree,
-                UI::Shell::{FOLDERID_Profile, SHGetKnownFolderPath},
-            },
-        };
+    use {
+        core::slice::from_raw_parts,
+        std::{
+            ffi::{c_void, OsString},
+            os::windows::prelude::OsStringExt,
+            ptr::null_mut,
+        },
+        windows_sys::Win32::{
+            Globalization::lstrlenW,
+            System::Com::CoTaskMemFree,
+            UI::Shell::{FOLDERID_Profile, SHGetKnownFolderPath},
+        },
+    };
 
-        /// Return the path of the user's home directory.
-        pub fn home_dir() -> Option<PathBuf> {
+    /// Return the path of the user's home directory.
+    pub fn home_dir() -> Option<std::path::PathBuf> {
+        if cfg!(target_family = "windows") {
             let mut path_ptr = null_mut();
             (unsafe { SHGetKnownFolderPath(&FOLDERID_Profile, 0, 0, &mut path_ptr) } == 0)
                 .then_some({
@@ -27,43 +26,24 @@ mod home_dir {
                     unsafe { CoTaskMemFree(path_ptr as *const c_void) }
                     ostr.into()
                 })
+        } else {
+            std::env::var_os("HOME").map(Into::into)
         }
     }
-    #[cfg(target_family = "windows")]
-    pub use home_dir_windows::*;
-
-    #[cfg(not(target_family = "windows"))]
-    mod home_dir_ne_windows {
-        use std::{env::var_os, path::PathBuf};
-        const HOME: &'static str = "HOME";
-
-        /// Return the path of the user's home directory.
-        pub fn home_dir() -> Option<PathBuf> {
-            var_os(HOME).map(Into::into)
-        }
-    }
-    #[cfg(not(target_family = "windows"))]
-    pub use home_dir_ne_windows::*;
 }
 #[cfg(feature = "home_dir")]
 pub use home_dir::*;
 
 #[cfg(feature = "expand_tilde")]
 mod expand_tilde {
-    use {
-        super::home_dir,
-        std::path::{Path, PathBuf},
-    };
-    const TILDE: &'static str = "~";
-
     /// Expand the tilde (`~`) from within the provided path.
-    pub fn expand_tilde(path: impl AsRef<Path>) -> Option<PathBuf> {
+    pub fn expand_tilde(path: impl AsRef<std::path::Path>) -> Option<std::path::PathBuf> {
         let p = path.as_ref();
 
-        Some(if p.starts_with(TILDE) {
-            let mut home = home_dir()?;
+        Some(if p.starts_with("~") {
+            let mut home = super::home_dir()?;
 
-            if !p.ends_with(TILDE) {
+            if !p.ends_with("~") {
                 let mut cmpts = p.components();
                 cmpts.next()?;
                 home.extend(cmpts);
@@ -79,21 +59,17 @@ pub use expand_tilde::*;
 
 #[cfg(feature = "test")]
 mod test {
-    mod expand_tilde {
-        #[test]
-        fn expand_tilde_test() {
-            let expected = dirs::home_dir().unwrap().join("foo");
-            let expanded = crate::expand_tilde("~/foo").unwrap();
-            assert_eq!(expanded, expected)
-        }
+    #[test]
+    fn expand_tilde_test() {
+        let expected = dirs::home_dir().unwrap().join("foo");
+        let resulted = crate::expand_tilde("~/foo").unwrap();
+        assert_eq!(resulted, expected)
     }
 
-    mod home_dir {
-        #[test]
-        fn home_dir_test() {
-            let expected = dirs::home_dir().unwrap();
-            let home_dir = crate::home_dir().unwrap();
-            assert_eq!(home_dir, expected)
-        }
+    #[test]
+    fn home_dir_test() {
+        let expected = dirs::home_dir().unwrap();
+        let resulted = crate::home_dir().unwrap();
+        assert_eq!(resulted, expected)
     }
 }
